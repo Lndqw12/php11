9d44e75160b0905cf5078e717a7911bdfcbfff2b
Lndqw12 <COMP222012016@mail.ru>
2016-09-09 16:08:47 +0300

    phhp

---
+++ index1.php
@@ -0,0 +1,18 @@
+﻿<meta charset ="utf-8">
+<?php
+ //$int = 10;
+// $str = "10";
+	//if($int==$str)
+		//echo "они равны";
+	//else
+	//	echo "они не равны";
+		
+//echo "\xFF","\xA1","\xBS","\xBEL","\xACK";	
+		
+ $a = " ";
+ $b = 0;
+ if($a===$b)
+		echo "они равны";
+	else
+		echo "они не равны";
+ ?>
